fc795223811ed11d6f5baca582d2a14c9cfe1655
Paulo Humberto Rezende <paulo.rezende@ifsuldeminas.edu.br>
2019-12-24 13:11:06 -0300

    padrão de projeto State

--- orcamento.py
+++ orcamento.py
@@ -1,15 +1,110 @@
+
+from abc import ABCMeta, abstractmethod
+class Estado_de_um_orcamento(object, metaclass=ABCMeta):
+
+	@abstractmethod
+	def aplica_desconto_extra(self, orcamento):
+		pass
+
+	@abstractmethod
+	def aprova(self, orcamento):
+		pass
+
+	@abstractmethod
+	def reprova(self, orcamento):
+		pass
+
+	@abstractmethod
+	def finaliza(self, orcamento):
+		pass
+
+
+class Em_aprovacao(Estado_de_um_orcamento):
+
+	def aplica_desconto_extra(self, orcamento):
+		orcamento.adiciona_desconto_extra(orcamento.valor * 0.02)
+
+	def aprova(self, orcamento):
+		orcamento.estado_atual = Aprovado()
+
+	def reprova(self, orcamento):
+		orcamento.estado_atual = Reprovado()
+
+	def finaliza(self, orcamento):
+		raise Exception("Orçamento em aprovação não pode ser finalizado.")
+
+class Aprovado(Estado_de_um_orcamento):
+
+	def aplica_desconto_extra(self, orcamento):
+		orcamento.adiciona_desconto_extra(orcamento.valor * 0.05) 
+
+	def aprova(self, orcamento):
+		raise Exception("Orçamento já está com estado aprovado.")
+
+	def reprova(self, orcamento):
+		orcamento.estado_atual = Reprovado()
+
+	def finaliza(self, orcamento):
+		orcamento.estado_atual = Finalizado()
+
+
+class Reprovado(Estado_de_um_orcamento):
+
+	def aplica_desconto_extra(self, orcamento):
+		raise Exception("Orçamentos reprovados não receberão desconto extra.")
+
+	def aprova(self, orcamento):
+		orcamento.estado_atual = Aprovado()
+
+	def reprova(self, orcamento):
+		raise Exception("Orçamento já está com estado reprovado.")
+
+	def finaliza(self, orcamento):
+		orcamento.estado_atual = Finalizado()
+
+class Finalizado(Estado_de_um_orcamento):
+
+	def aplica_desconto_extra(self, orcamento):
+		raise Exception("Orçamentos finalizados não receberão desconto extra.")
+
+	def aprova(self, orcamento):
+		raise Exception("Orçamentos finalizados não podem ser aprovados.")
+
+	def reprova(self, orcamento):
+		raise Exception("Orçamentos finalizados não podem ser reprovados.")
+
+	def finaliza(self, orcamento):
+		raise Exception("O estado atual do orçamento já é finalizado.")
+
 class Orcamento(object):
 
 	def __init__(self):
 
 		self.__itens = []
+		self.estado_atual = Em_aprovacao()
+		self.__desconto_extra = 0
+
+	def aprova(self):
+		self.estado_atual.aprova(orcamento)
+
+	def reprova(self):
+		self.estado_atual.reprova(orcamento)
+
+	def finaliza(self):
+		self.estado_atual.finaliza(orcamento)
+
+	def aplica_desconto_extra(self):
+		self.estado_atual.aplica_desconto_extra(self)	
+	
+	def adiciona_desconto_extra(self, desconto):
+		self.__desconto_extra += desconto
 
 	@property 
 	def valor(self):
 		total = 0.0
 		for item in self.__itens:
 			total += item.valor
-		return total
+		return total - self.__desconto_extra
 
 	def obter_itens(self):
 		return tuple(self.__itens)
@@ -37,6 +132,19 @@ class Item(object):
 	@property
 	def nome(self):
 		return self.__nome
+
+if __name__ == "__main__":
+
+	orcamento = Orcamento()
+	orcamento.adiciona_item(Item('Item-1',100))
+	orcamento.adiciona_item(Item('Item-1',50))
+	orcamento.adiciona_item(Item('Item-1',400))
+
+	print(orcamento.valor)
+	orcamento.aprova()
+	orcamento.aplica_desconto_extra()
+	print(orcamento.valor)
+
 
 	
 	
